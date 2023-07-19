9a170f1ed216b97fa5ebb1d737defaa8836bb86e
Tharinda Pamindu <tharipami2k20@gmail.com>
2023-07-19 20:27:22 +0530

    State Handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCounter = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Counter : {count}</h2>
+        <div>
+          <button type="button" onClick={increaseCounter}>
+            Increase
+          </button>
+          <button type="button" onClick={decreaseCount}>
+            Decrease
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
